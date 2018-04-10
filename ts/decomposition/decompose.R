908f434f5b260dc3d7b26269221e17d80dc7f114
Subhash Chandran <subwiz@gmail.com>
2018-04-10 19:12:41 +0530

    Data from GitHub source.

--- ts/decomposition/decompose.R
+++ ts/decomposition/decompose.R
@@ -1,8 +1,7 @@
 # Example from: http://a-little-book-of-r-for-time-series.readthedocs.io/en/latest/src/timeseries.html
 
-births <- scan("http://robjhyndman.com/tsdldata/data/nybirths.dat")
+births <- scan("https://raw.githubusercontent.com/subwiz/r-playground/master/ts/decomposition/nybirths.dat")
 birthstimeseries <- ts(births, frequency=12, start=c(1946,1))
 plot.ts(birthstimeseries)
 birthstimeseriescomponents <- decompose(birthstimeseries)
 plot(birthstimeseriescomponents)
-?scan
