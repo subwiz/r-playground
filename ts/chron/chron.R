2655378076d900a5c06f0f57d1a3534427ca034f
Subhash Chandran <subwiz@gmail.com>
2018-04-11 21:27:50 +0530

    Managing date-time values using `chron` package.

---
+++ ts/chron/chron.R
@@ -0,0 +1,7 @@
+library("chron")
+
+x <- read.csv("https://raw.githubusercontent.com/subwiz/r-playground/master/ts/chron/data.csv", header=TRUE)
+x$tm <- as.character(x$time)
+dtparts <- t(as.data.frame(strsplit(x$tm,"T")))
+x$chron <- chron(dates=dtparts[,1],times=dtparts[,2],format=c('y-m-d','h:m:s'))
+plot(x$chron, x$val, xlab="Time", ylab="CPU", col="black", type='l')
