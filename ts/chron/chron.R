8c6a87c7250595c816d7b7d4e6cbfd6758d7a680
Subhash Chandran <subwiz@gmail.com>
2018-04-11 21:29:25 +0530

    Value added.

--- ts/chron/chron.R
+++ ts/chron/chron.R
@@ -4,4 +4,4 @@ x <- read.csv("https://raw.githubusercontent.com/subwiz/r-playground/master/ts/c
 x$tm <- as.character(x$time)
 dtparts <- t(as.data.frame(strsplit(x$tm,"T")))
 x$chron <- chron(dates=dtparts[,1],times=dtparts[,2],format=c('y-m-d','h:m:s'))
-plot(x$chron, x$val, xlab="Time", ylab="CPU", col="black", type='l')
+plot(x$chron, x$val, xlab="Time", ylab="Value", col="black", type='l')
